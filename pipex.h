1468db1e48d83fe2c773cbe5335614e1294f7c5d
Your Name <you@example.com>
2025-01-02 10:02:48 +0100

    first setup without libft

---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tleister <tleister@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 09:52:50 by tleister          #+#    #+#             */
+/*   Updated: 2025/01/02 10:02:18 by tleister         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
